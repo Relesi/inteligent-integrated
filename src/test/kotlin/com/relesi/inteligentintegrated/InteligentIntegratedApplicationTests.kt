c71a25f465c55362fc27bf6872f0f27de559966c
Renato Lessa <renatolessa@Renatos-MacBook-Pro.local>
2021-05-10 18:28:54 -0300

    service startup

---
+++ src/test/kotlin/com/relesi/inteligentintegrated/InteligentIntegratedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.relesi.inteligentintegrated
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InteligentIntegratedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
