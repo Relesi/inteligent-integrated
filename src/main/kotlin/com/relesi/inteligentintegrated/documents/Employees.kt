edf482ebbe46b8c1b777fc1bff4e60175b501a40
Renato Lessa <renatolessa@Renatos-MacBook-Pro.local>
2021-05-11 19:41:45 -0300

    create document to employees

---
+++ src/main/kotlin/com/relesi/inteligentintegrated/documents/Employees.kt
@@ -0,0 +1,21 @@
+package com.relesi.inteligentintegrated.documents
+
+import com.relesi.inteligentintegrated.enums.ProfileEnum
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
+data class Employees (
+
+    val name: String,
+    val email: String,
+    val password: String,
+    val ssn: String,
+    val profile: ProfileEnum,
+    val companyId: String,
+    val hourValue: Double? = 0.0,
+    val qtyHoursWorkedDay: Float? = 0.0f,
+    val qtyLunchHours: Float? = 0.0f,
+    @Id val id: String? = null
+
+)
