66409797de11b5e00bf688aa5d07a38c7588ce5a
Renato Lessa <renatolessa@Renatos-MacBook-Pro.local>
2021-05-12 17:47:10 -0300

    create repository mongoDb

---
+++ src/main/kotlin/com/relesi/inteligentintegrated/repositories/EmployeesRepository.kt
@@ -0,0 +1,12 @@
+package com.relesi.inteligentintegrated.repositories
+
+import com.relesi.inteligentintegrated.documents.Company
+import com.relesi.inteligentintegrated.documents.Employees
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface EmployeesRepository : MongoRepository<Employees, String> {
+
+    fun findByEmail(email: String): Employees?
+
+    fun findBySsn(ssn: String): Employees?
+}
