8468f502e1619054c5f32d602cf6f92b304921ec
Renato Lessa <renatolessa@Renatos-MacBook-Pro.local>
2021-05-15 10:46:34 -0300

    implement service company

---
+++ src/main/kotlin/com/relesi/inteligentintegrated/services/impl/CompanyServiceImpl.kt
@@ -0,0 +1,16 @@
+package com.relesi.inteligentintegrated.services.impl
+
+import com.relesi.inteligentintegrated.documents.Company
+import com.relesi.inteligentintegrated.repositories.CompanyRepository
+import com.relesi.inteligentintegrated.services.CompanyService
+import org.springframework.stereotype.Service
+
+@Service
+class CompanyServiceImpl(val companyRepository: CompanyRepository): CompanyService {
+
+    override fun searchByEin(ein: String): Company? = companyRepository.findByEin(ein)
+
+    override fun persist(company: Company): Company = companyRepository.save(company)
+
+
+}
