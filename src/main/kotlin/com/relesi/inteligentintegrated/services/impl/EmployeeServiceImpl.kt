05359dc4d30087b8ed914ce8577b2b8165e4b829
Renato Lessa <renatolessa@Renatos-MacBook-Pro.local>
2021-05-19 19:21:29 -0300

    implament methods to tdd app

--- src/main/kotlin/com/relesi/inteligentintegrated/services/impl/EmployeeServiceImpl.kt
+++ src/main/kotlin/com/relesi/inteligentintegrated/services/impl/EmployeeServiceImpl.kt
@@ -1,23 +1,20 @@
 package com.relesi.inteligentintegrated.services.impl
 
-import com.relesi.inteligentintegrated.documents.Employees
-import com.relesi.inteligentintegrated.services.EmployeesService
+import com.relesi.inteligentintegrated.documents.Employee
+import com.relesi.inteligentintegrated.repositories.EmployeeRepository
+import com.relesi.inteligentintegrated.services.EmployeeService
+import org.springframework.stereotype.Service
 
-class EmployeeServiceImpl : EmployeesService {
+@Service
+class EmployeeServiceImpl (val employeeRepository: EmployeeRepository): EmployeeService {
 
-    override fun persist(employees: Employees): Employees {
-        TODO("Not yet implemented")
-    }
+    override fun persist(employee: Employee): Employee = employeeRepository.save(employee)
 
-    override fun searchBySsn(ssn: String): Employees? {
-        TODO("Not yet implemented")
-    }
+    override fun searchBySsn(ssn: String): Employee? = employeeRepository.findBySsn(ssn)
 
-    override fun searchByEmail(email: String): Employees? {
-        TODO("Not yet implemented")
-    }
+    override fun searchByEmail(email: String): Employee? = employeeRepository.findByEmail(email)
 
-    override fun searchById(id: String): Employees? {
-        TODO("Not yet implemented")
-    }
+    override fun searchById(id: String): Employee? = employeeRepository.findById(id).get()
+
+
 }
