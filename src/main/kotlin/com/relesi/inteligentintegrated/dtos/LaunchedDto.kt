ac405e7eaaf59cf8f485bd3bab2dc2e7c0c89735
Renato Lessa <renatolessa@Renatos-MacBook-Pro.local>
2021-05-24 18:20:33 -0300

    implment dtos

---
+++ src/main/kotlin/com/relesi/inteligentintegrated/dtos/LaunchedDto.kt
@@ -0,0 +1,17 @@
+package com.relesi.inteligentintegrated.dtos
+
+import javax.validation.constraints.NotEmpty
+
+class LaunchedDto (
+    @get:NotEmpty(message = "Date cannot be empty...")
+    val date: String? = null,
+
+    @get:NotEmpty(message = "Type cannot be empty...")
+    val type: String? = null,
+
+    val employeesId: String? = null,
+    val description: String? = null,
+    val localization: String? = null,
+    val id: String? = null
+)
+
