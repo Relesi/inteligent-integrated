478c9d7c678d104794ac164ebe8a860b49d01460
Renato Lessa <renatolessa@Renatos-MacBook-Pro.local>
2021-06-01 13:09:22 -0300

    imple controller launched

--- src/main/kotlin/com/relesi/inteligentintegrated/controller/LaunchedController.kt
+++ src/main/kotlin/com/relesi/inteligentintegrated/controller/LaunchedController.kt
@@ -1,5 +1,6 @@
 package com.relesi.inteligentintegrated.controller
 
+import com.relesi.inteligentintegrated.documents.Employee
 import com.relesi.inteligentintegrated.documents.Launched
 import com.relesi.inteligentintegrated.dtos.LaunchedDto
 import com.relesi.inteligentintegrated.response.Response
@@ -11,10 +12,13 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.ObjectError
 
 import org.springframework.web.bind.annotation.*
 
 import java.text.SimpleDateFormat
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/launched")
@@ -57,6 +61,34 @@ class LaunchedController(val launchedService: LaunchedService, val employeeServi
         }
         response.date = converterLaunchedDto(launched)
         return ResponseEntity.ok(response)
+    }
+
+    @PostMapping
+    fun add(@Valid @RequestBody launchedDto: LaunchedDto, result: BindingResult): ResponseEntity<Response<LaunchedDto>>{
+        val response: Response<LaunchedDto> = Response<LaunchedDto>()
+        validateEmployee(launchedDto, result)
+
+        if (result.hasErrors()){
+            //for (errors in result.allErrors) response.errors.add(errors.defaultMessage)
+            result.allErrors.forEach { errors -> errors.defaultMessage?.let { response.errors.add(it) } }
+            return ResponseEntity.badRequest().body(response)
+        }
+
+        val launched: Launched = converterLaunchedDto(launchedDto, result)
+        launchedService.persist(launched)
+        response.date = converterLaunchedDto(launched)
+        return ResponseEntity.ok(response)
+    }
+
+    private fun validateEmployee(launchedDto: LaunchedDto, result: BindingResult) {
+        if (launchedDto.employeesId == null) {
+            result.addError(ObjectError("employee", "Employee not informed..."))
+            return
+        }
+        val employee: Employee? = employeeService.searchById(launchedDto.employeesId)
+        if (employee == null){
+            result.addError(ObjectError("employee", "Employee not found. non-existent ID."));
+        }
     }
 
     private fun converterLaunchedDto(launched: Launched): LaunchedDto =
