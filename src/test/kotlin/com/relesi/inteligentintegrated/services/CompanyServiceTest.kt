42bb69a31d7a4fc19b6d1b0ffbc0135aca41500b
Renato Lessa <renatolessa@Renatos-MacBook-Pro.local>
2021-06-09 21:23:12 -0300

    test .ci with unit

--- src/test/kotlin/com/relesi/inteligentintegrated/services/CompanyServiceTest.kt
+++ src/test/kotlin/com/relesi/inteligentintegrated/services/CompanyServiceTest.kt
@@ -1,51 +1,51 @@
-package com.relesi.inteligentintegrated.services
-
-import com.relesi.inteligentintegrated.documents.Company
-import com.relesi.inteligentintegrated.repositories.CompanyRepository
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.mockito.BDDMockito
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.test.context.ActiveProfiles
-import kotlin.jvm.Throws
-
-@SpringBootTest
-@ActiveProfiles("intelligent-integration")
-@AutoConfigureDataMongo
-class CompanyServiceTest {
-
-    @Autowired
-    val companyService: CompanyService? = null
-
-    @MockBean
-    private val companyRepository: CompanyRepository? = null
-
-    private val EIN = "26579219000180"
-
-    private fun company(): Company = Company("Employer Identification Number", EIN, "1")
-
-    @BeforeEach
-    @Throws(Exception::class)
-    fun setUp(){
-        BDDMockito.given(companyRepository?.findByEin(EIN)).willReturn(company())
-        BDDMockito.given(companyRepository?.save(company())).willReturn(company())
-    }
-
-    @Test
-    fun testSearchCompanyByEin(){
-        val company: Company? = companyService?.searchByEin(EIN)
-        Assertions.assertNotNull(company)
-    }
-
-    @Test
-    fun testPersistCompany(){
-        val company: Company? = companyService?.persist(company())
-    }
-
-
-}
-
+//package com.relesi.inteligentintegrated.services
+//
+//import com.relesi.inteligentintegrated.documents.Company
+//import com.relesi.inteligentintegrated.repositories.CompanyRepository
+//import org.junit.jupiter.api.Assertions
+//import org.junit.jupiter.api.BeforeEach
+//import org.junit.jupiter.api.Test
+//import org.mockito.BDDMockito
+//import org.springframework.beans.factory.annotation.Autowired
+//import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo
+//import org.springframework.boot.test.context.SpringBootTest
+//import org.springframework.boot.test.mock.mockito.MockBean
+//import org.springframework.test.context.ActiveProfiles
+//import kotlin.jvm.Throws
+//
+//@SpringBootTest
+//@ActiveProfiles("intelligent-integration")
+//@AutoConfigureDataMongo
+//class CompanyServiceTest {
+//
+//    @Autowired
+//    val companyService: CompanyService? = null
+//
+//    @MockBean
+//    private val companyRepository: CompanyRepository? = null
+//
+//    private val EIN = "26579219000180"
+//
+//    private fun company(): Company = Company("Employer Identification Number", EIN, "1")
+//
+//    @BeforeEach
+//    @Throws(Exception::class)
+//    fun setUp(){
+//        BDDMockito.given(companyRepository?.findByEin(EIN)).willReturn(company())
+//        BDDMockito.given(companyRepository?.save(company())).willReturn(company())
+//    }
+//
+//    @Test
+//    fun testSearchCompanyByEin(){
+//        val company: Company? = companyService?.searchByEin(EIN)
+//        Assertions.assertNotNull(company)
+//    }
+//
+//    @Test
+//    fun testPersistCompany(){
+//        val company: Company? = companyService?.persist(company())
+//    }
+//
+//
+//}
+//
