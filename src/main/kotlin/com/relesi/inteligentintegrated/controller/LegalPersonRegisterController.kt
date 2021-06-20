535bd06409e5e7f199fb0493f9287dec132213e8
Renato Lessa <renatolessa@Renatos-MacBook-Pro.local>
2021-06-20 13:20:21 -0300

    implement contreoller LegalPersonRegister

--- src/main/kotlin/com/relesi/inteligentintegrated/controller/LegalPersonRegisterController.kt
+++ src/main/kotlin/com/relesi/inteligentintegrated/controller/LegalPersonRegisterController.kt
@@ -1,4 +1,26 @@
 package com.relesi.inteligentintegrated.controller
 
-class LegalPersonRegisterController {
+import com.relesi.inteligentintegrated.dtos.LegalPersonRegisterDto
+import com.relesi.inteligentintegrated.response.Response
+import com.relesi.inteligentintegrated.services.CompanyService
+import com.relesi.inteligentintegrated.services.EmployeeService
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/api/legal-person")
+class LegalPersonRegisterController(val companyService: CompanyService,
+                                  val employeeService: EmployeeService ){
+
+    fun register(@Valid @RequestBody legalPersonRegisterDto: LegalPersonRegisterDto,
+    result: BindingResult): ResponseEntity<Response<LegalPersonRegisterDto>>{
+        val response: Response<LegalPersonRegisterDto> = Response<LegalPersonRegisterDto>()
+         
+    }
+
+
 }
